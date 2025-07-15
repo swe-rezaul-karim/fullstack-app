dccc55612b47e082dc2263fff0ac65b3eb8c781e
Rezaul Karim <rezaul@Rezauls-MacBook-Pro.local>
2025-07-15 23:37:11 +0600

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,7 @@
-import { useEffect, useState } from "react";
+import React from 'react';
 
 function App() {
-  const [msg, setMsg] = useState("");
-
-  useEffect(() => {
-    fetch("/")
-      .then((res) => res.text())
-      .then((data) => setMsg(data));
-  }, []);
-
-  return <h1>{msg}</h1>;
+  return <h1>Hello from React!</h1>;
 }
 
 export default App;
